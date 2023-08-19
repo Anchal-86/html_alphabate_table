7e2e434a0fcf1a4bd43b86225c52674308b29397
anchalDev159357 <singhanchal159357@gmail.com>
2023-08-19 20:58:51 +0530

    aaded changes

---
+++ script.js
@@ -0,0 +1,8 @@
+const alphabate = document.querySelectorAll('.alpha');
+const Explain = document.querySelectorAll('.Explore');
+
+
+for (let i = 0; i < Explain.length; i++) {
+    console.log(Explain[i].textContent)
+    
+}

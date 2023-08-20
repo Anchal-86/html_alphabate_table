92ed7f8e57cc3d6461ccf67b2966b60cc598b72e
anchalDev159357 <singhanchal159357@gmail.com>
2023-08-20 07:23:19 +0530

    added changes

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 const alphabate = document.querySelectorAll(".alpha");
-const Explain = document.querySelectorAll(".Explore");
+const Explain = document.querySelectorAll(".Explain");
 
 for (let i = 0; i < Explain.length; i++) {
   Explain[i].addEventListener('onclick' ,function(){
